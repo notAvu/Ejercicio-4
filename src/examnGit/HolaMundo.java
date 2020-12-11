0794585df11ce977ff084567d3dddebfd5ecc0ac
Millia <EmiliaRabia@ggmail.com>
2020-12-11 14:19:35 +0100

    commit ya queda menos

--- src/examnGit/HolaMundo.java
+++ src/examnGit/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo
 {
 public static void main (String[]args) 	
 {
-System.out.println("Hola Mundo!");
+System.out.println("Quiero que lleguen ya las vacaciones");
+System.out.println("Ya queda menos");
 }
 }
